5f60a91b9fb60c49d183ac90383f903fe48ca0f6
LinkinStar <linkinstar@foxmail.com>
2018-06-06 15:34:43 +0800

    1、修改数据库连接时区为上海 2、让thymeleaf解析标签没有闭合时通过 3、添加SpringBootVFS 4、新增配置上传文件限制大小

--- src/main/java/com/linkinstars/springBootTemplate/config/MyBatisConfig.java
+++ src/main/java/com/linkinstars/springBootTemplate/config/MyBatisConfig.java
@@ -3,6 +3,7 @@ package com.linkinstars.springBootTemplate.config;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -26,6 +27,7 @@ public class MyBatisConfig {
     public SqlSessionFactoryBean sqlSessionFactory(ApplicationContext applicationContext) throws Exception {
         SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
         sessionFactory.setDataSource(dataSource);
+        sessionFactory.setVfs(SpringBootVFS.class);
         sessionFactory.setTypeAliasesPackage("com.linkinstars.springBootTemplate.bean");
         sessionFactory.setMapperLocations(applicationContext.getResources("classpath*:mapper/*.xml"));
         return sessionFactory;
